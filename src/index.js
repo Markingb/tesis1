dbb3a24af1fe7b271439b63dbe678eef924f1362
Martin <martinreyes501@gmail.com>
2023-06-05 07:23:28 -0700

    antes de mandar a bd

--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ const multer = require('multer');
 const fs= require('fs');
 const pdfparse = require('pdf-parse');
 
-const pdffile = fs.readFileSync('src/uploads/ConstanciaKardex.pdf');
+//const pdffile = fs.readFileSync('src/uploads/ConstanciaKardex.pdf');
 
-
+//para definir donde se guarda y con que nombre se guarda
 const storage = multer.diskStorage({
     destination: path.join(__dirname, 'uploads'),
     filename: (req, file, cb) => {
@@ -50,21 +50,90 @@ app.use((req, res, next) => {
 function convertir(){
     
 }
-//Rutas
-app.use(require('./routes/index.js'));
-//app.use(require('./routes/authentication.js'));
-app.post('/upload', (req, res) =>{
-    res.send('Subido');
-    console.log(storage.destination);
+//funciópn para crear txt
+function text_(archiv0){
+    concatenado = 'src/uploads/'+archiv0+'.pdf';
+    //console.log(concatenado);
+    const pdffile = fs.readFileSync(concatenado);
     pdfparse(pdffile).then(function (data){
-        fs.writeFile('src/uploads/archivo.txt', data.text, 'utf8', (error) => {
+        
+        fs.writeFile('src/uploads/'+archiv0+'.txt', data.text, 'utf8', (error) => {
             if (error) {
               console.error('Error al crear el archivo:', error);
             } else {
               console.log('Archivo creado exitosamente.');
+              const ruta_send = 'src/uploads/'+archiv0+'.txt';
+              console.log(ruta_send);
+              extraercadenas(ruta_send);
             }
           });
     });
+}
+
+//funcion para extraer y mandar a la bd
+function extraercadenas(ruta){
+    //const rutaTxt =;
+    
+    fs.readFile(ruta, 'utf8', (err, data) => {
+        if (err) {
+          console.error('Error al leer el archivo:', err);
+          return;
+        }
+      
+        // Separamos el archivo 
+        const lineas = data.split('\n');
+
+        //rellenamos las variables principales
+        const facultad = lineas[10].slice(5,(lineas[10].length));
+        const carrera = lineas[11].slice(3,(lineas[10].length));
+        const n_cuenta = lineas[12].slice(0,9);
+        const nombre = lineas[12].slice(10,(lineas[12].length));
+        const promedio = lineas[15];
+        const plan_estudio = lineas[13].slice((lineas[13].length-1),(lineas[13].length));
+        const semestre = lineas[lineas.length-11];
+        const primer_a = lineas[lineas.length-10].slice((([lineas.length-10].length-13)),([lineas.length-10].length-8));
+        const seg_a = lineas[lineas.length-10].slice((([lineas.length-10].length-5)),(([lineas.length-10]).length-1));
+        //console.log(seg_a);
+        const generacion = primer_a + "-" + seg_a;
+        //console.log(generacion);
+        const lineaEspecifica = lineas[16 - 1];
+        //console.log(lineaEspecifica);
+
+        //recorremos las materias
+        var semestre_actual = 1;
+        for (var i = 17; i < ((lineas.length)-11); i++) {
+            if(lineas[i].length < 2){
+                semestre_actual = semestre_actual + 1;
+            }else{
+                const clave_materia = lineas[i].split(0,4);
+                
+            }
+          }
+          
+
+
+        //console.log('Contenido del archivo:', data);
+      });
+}
+
+//Rutas
+app.use(require('./routes/index.js'));
+//app.use(require('./routes/authentication.js'));
+app.post('/upload', (req, res) =>{
+    //res.send('Subido');
+    //const upload = multer({ storage: storage });
+    const nombreArchivo = req.file.originalname;
+    const nombreextraido = nombreArchivo.substring(0, nombreArchivo.length-4);
+    //console.log(nombreextraido);
+    
+    text_(nombreextraido);
+    //extraercadenas(nombreextraido+'.txt');
+    
+    
+    
+    
+    res.send('Se ha subido correctamente');
+    //console.log(nombreArchivo);
 });
 
 //Publiic 
